ee2ebeace56f0a7aebedb15c7802575c0e3d156e
Jonas-Victor950 <Jonasvpsouza950@gmail.com>
2025-04-29 13:46:06 -0300

    feat: specific repositorie to Categorias

---
+++ Repositories/ICategoriaRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APICatalogo.Models;
+
+namespace APICatalogo.Repositories;
+
+public interface ICategoriaRepository
+{
+    IEnumerable<Categoria> GetCategorias();
+    Categoria GetCategoria(int id);
+    Categoria Create(Categoria categoria);
+    Categoria Update(Categoria categoria);
+    Categoria Delete(int id);
+}
