a15a62190b24aa636842082d3b9b66b71110873f
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2025-05-02 22:23:54 -0300

    feat: Generic Repository

--- Repositories/ICategoriaRepository.cs
+++ Repositories/ICategoriaRepository.cs
@@ -1,16 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using APICatalogo.Models;
 
 namespace APICatalogo.Repositories;
 
-public interface ICategoriaRepository
+public interface ICategoriaRepository : IRepository<Categoria>
 {
-    IEnumerable<Categoria> GetCategorias();
-    Categoria GetCategoria(int id);
-    Categoria Create(Categoria categoria);
-    Categoria Update(Categoria categoria);
-    Categoria Delete(int id);
 }
