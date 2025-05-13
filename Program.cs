3b58c23daa029571c6a53b28ae5dcba759d6aa17
Jonas-Victor950 <Jonasvpsouza950@gmail.com>
2025-05-13 12:38:21 -0300

    feat: auth politics

--- Program.cs
+++ Program.cs
@@ -85,6 +85,17 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("SuperAdminOnly", policy => policy.RequireRole("Admin").RequireClaim("id", "Jonas"));
+    options.AddPolicy("UserOnly", policy => policy.RequireRole("User"));
+    options.AddPolicy("ExclusivePolicyOnly", policy =>
+    {
+        policy.RequireAssertion(context => context.User.HasClaim(claim => claim.Type == "id" && claim.Value == "Jonas") || context.User.IsInRole("SuperAdmin"));
+    });
+});
+
 builder.Services.AddTransient<IMeuServico, MeuServico>();
 builder.Services.Configure<ApiBehaviorOptions>(options =>
 {
